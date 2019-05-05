90780f0f19a7dc1b3896f903f0f5ebbdc37f4776
Luan G <luangrezende@gmail.com>
2019-05-05 16:11:32 -0300

    upload project

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        protected readonly dbContextApi db;
+        public ValuesController(dbContextApi _db) => db = _db;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return db.User.ToList();
+        }
+
+        // GET api/values/id
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return db.User.Find(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] User usuario)
+        {
+            db.User.Add(usuario);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ dbContextApi.cs
@@ -0,0 +1,35 @@
+﻿using ApiTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTutorial
+{
+    public class dbContextApi : DbContext
+    {
+        //CLASSES
+        public DbSet<User> User { get; set; }
+
+        public dbContextApi(DbContextOptions<dbContextApi> options) : base(options) { }
+        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<dbContextApi>
+        {
+            public dbContextApi CreateDbContext(string[] args)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var builder = new DbContextOptionsBuilder<dbContextApi>();
+                var connectionString = configuration.GetConnectionString("DefaultConecction");
+                builder.UseSqlServer(connectionString);
+                return new dbContextApi(builder.Options);
+            }
+        }
+    }
+}
